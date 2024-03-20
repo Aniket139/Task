93e3ee6fa03fd96488591539267ae0665a55c26e
Aniket139 <aniketpatel139@gamil.com>
2024-03-20 13:12:27 +0530

    minecartshop

---
+++ app/controllers/admins/dashboard_controller.rb
@@ -0,0 +1,12 @@
+class Admins::DashboardController < ApplicationController
+  def index
+    @categories = Category.all.count
+    @products = Product.all.count
+    @orders = Order.all
+    @total_revenew = @orders.includes(:product)
+    @profit = []
+    @total_revenew.each do |price|
+      @profit << price.product.sell_price - price.product.purchase_price
+    end
+  end
+end
